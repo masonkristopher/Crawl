bee29ab76cf95a27049299342e45908bdccf0c3f
Richard Rosier <jimmyroro58@gmail.com>
2020-06-10 14:12:51 -0500

    change PORT import in vue.config

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,11 +1,11 @@
-const PORT = require('../server/index')
+const { PORT } = require('../server/index')
 
 module.exports = {
   devServer: {
     public: `13.66.15.121:${PORT}`,
   }
 };
-
+console.log(PORT);
 // const path = require("path");
 // module.exports = {
 //   outputDir: path.resolve(__dirname, "../public"),

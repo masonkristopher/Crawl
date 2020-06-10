f94790f763d4628ca1b6e34894a1c9356da194e1
Richard Rosier <jimmyroro58@gmail.com>
2020-06-10 13:23:04 -0500

    add devServer to vue.config

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,17 @@
+// module.exports = {
+//   devServer: {
+//     public: '13.66.15.121',
+//   }
+// };
+
+const path = require("path");
 module.exports = {
+  outputDir: path.resolve(__dirname, "../public"),
   devServer: {
-    public: '13.66.15.121',
-  }
+    proxy: {
+      "/api": {
+        target: "http://localhost:8081",
+      },
+    },
+  },
 };

5d30fc9e2c620dd67229ad8bb83a5f661ae021f0
Richard Rosier <jimmyroro58@gmail.com>
2020-06-12 16:47:07 -0500

    add ugly navbar, remove css from helloworld

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
-    <img id="header-img" alt="CRAWL logo" src="./assets/logo.png">
-    <HelloWorld msg="poop lol"/>
+    <div>
+      <NavBar/>
+    </div>
+    <div>
+      <HelloWorld msg="poop lol"/>
+    </div>    
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'App',
   components: {
+    NavBar,
     HelloWorld
   }
 }

964ec0b05482c7f906de1dc2aecfa512838e2b96
NaseerHines <Hinesnaseer@gmail.com>
2020-06-12 10:34:23 -0500

    commented out locationRouter needs to be completed

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const { testRouter } = require('./test');
 const { userRouter } = require('./user');
 const { crawlRouter } = require('./crawl');
-const { locationRouter } = require('./location');
+// const { locationRouter } = require('./location');
 
 
 const apiRouter = Router();
@@ -10,7 +10,7 @@ const apiRouter = Router();
 apiRouter.use('/test', testRouter);
 apiRouter.use('/user', userRouter);
 apiRouter.use('/crawl', crawlRouter);
-apiRouter.use('./location', locationRouter);
+// apiRouter.use('./location', locationRouter);
 
 
 module.exports.apiRouter = apiRouter;

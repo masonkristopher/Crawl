4ce028b48a624cb56c5cbb9afc27293701631c64
NaseerHines <Hinesnaseer@gmail.com>
2020-06-12 00:54:22 -0500

    added userRouter file/route pathing

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,12 @@
 const { Router } = require('express');
 const { testRouter } = require('./test');
+const { userRouter } = require('./user');
+
 
 const apiRouter = Router();
 
 apiRouter.use('/test', testRouter);
+apiRouter.use('/user', userRouter);
+
 
 module.exports.apiRouter = apiRouter;

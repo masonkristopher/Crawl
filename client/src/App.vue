432ec53241f05a26b9423582248b454ffb7c6dcf
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-06-12 20:49:18 -0500

    Remove the boilerplate component HW.vue from the App.vue and the rendered page

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,16 +4,12 @@
       <NavBar/>
     </div>
     <div>
-      <HelloWorld msg="poop lol"/>
-    </div>
-    <div>
       <google-map />
     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import NavBar from './components/NavBar.vue'
 import GoogleMap from './components/GoogleMap'
 
@@ -21,7 +17,6 @@ export default {
   name: 'App',
   components: {
     NavBar,
-    HelloWorld,
     GoogleMap
   }
 }

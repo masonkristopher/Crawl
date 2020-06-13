c6def70ae8603201cb6a8afcc82fd459f946379d
JerryMcDonald <jandh62808@gmail.com>
2020-06-12 20:59:01 -0500

    added process.env variables

--- server/passport-setup.js
+++ server/passport-setup.js
@@ -1,14 +1,21 @@
-const passport = require('passport')
+const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('dotenv').config({ path: '../.env' });
+
 
 passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://www.example.com/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://www.example.com/auth/google/callback"
+},
+  function (accessToken, refreshToken, profile, done) {
+    User.findOrCreate({
+      googleId: profile.id
+    }, function (err, user) {
+      return done(err, user);
     });
   }
 ));
+
+
+

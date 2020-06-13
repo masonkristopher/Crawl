686b71a02484b9af6d391ccb088f4d036377f4b4
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-06-13 17:59:28 -0500

    Clean up and reorganize the sidebar, removing the extra headers

--- client/src/components/SideBar.vue
+++ client/src/components/SideBar.vue
@@ -10,7 +10,9 @@
 
         <h4>
           Jerry McDonald
-           <vs-sidebar-group title="...">
+          <vs-button color="primary" type="flat">...</vs-button>
+
+           <vs-sidebar-group title="User">
           <vs-sidebar-item index="1">
             555-555-5555
           </vs-sidebar-item>
@@ -21,44 +23,43 @@
         </h4>
 
       </div>
+
+      <vs-divider position="left">
+        Crawls
+      </vs-divider>
+
       <vs-sidebar-group title="My Crawls">
-        <vs-sidebar-item index="1">
-          Christmas Crawl 2k21
-        </vs-sidebar-item>
-        <vs-sidebar-group title="Crawls I'm A Part Of">
+          <vs-sidebar-item index="1">
+            Christmas Crawl 2k21
+          </vs-sidebar-item>
+        </vs-sidebar-group>
+
+
+        <vs-sidebar-group title="Crawls I've Joined">
           <vs-sidebar-item index="2.1">
             Naseer's 21st Birthday Bash
           </vs-sidebar-item>
           <vs-sidebar-item index="2.2">
             Mac's SuperBowl Crawl
           </vs-sidebar-item>
-        </vs-sidebar-group>
-        <vs-sidebar-item index="2">
-          History
-        </vs-sidebar-item>
-        <vs-sidebar-item index="3">
-          security
-        </vs-sidebar-item>
-        <vs-sidebar-item index="4">
-          Help
-        </vs-sidebar-item>
       </vs-sidebar-group>
 
 
-      <vs-divider icon="person" position="left">
-        User
+      <vs-divider position="left">
+        Other
       </vs-divider>
 
-      <vs-sidebar-item index="5" icon="verified_user">
-        Configurations
+      <vs-sidebar-item index="5">
+        Security
       </vs-sidebar-item>
-      <vs-sidebar-item index="6" icon="account_box">
-        Profile
+      <vs-sidebar-item index="6">
+        Help
       </vs-sidebar-item>
 
+
       <div class="footer-sidebar" slot="footer">
-        <vs-button icon="reply" color="danger" type="flat">log out</vs-button>
-        <vs-button icon="settings" color="primary" type="border"></vs-button>
+        <vs-button color="danger" type="flat">log out</vs-button>
+        <vs-button color="primary" type="border">settings</vs-button>
       </div>
 
     </vs-sidebar>

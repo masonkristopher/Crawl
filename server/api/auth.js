fd5d1dc7fc898b03a1816a8fd443552a653bdb48
JerryMcDonald <jandh62808@gmail.com>
2020-06-12 22:03:02 -0500

    added the basic auth routes to begin

--- server/api/auth.js
+++ server/api/auth.js
@@ -1,6 +1,27 @@
 const { Router } = require('express');
+const passport = require('passport');
 
 const authRouter = Router();
+
+authRouter.use(passport.initialize());
+authRouter.use(passport.session());
+
+authRouter.get('/failed', (req, res) => res.send('You failed to log in'));
+
+authRouter.get('/good', (req, res) => res.send(`Welcome ${req.user.email}`));
+
+
+authRouter.get('/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+
+authRouter.get('/google/callback',
+  passport.authenticate('google', { failureRedirect: '/auth/failed' }),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect('/auth/good');
+  });
+
 
 module.exports = {
   authRouter,

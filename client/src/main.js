db1119ebf8470e8135ac0b8990557c495dc396fd
JerryMcDonald <jandh62808@gmail.com>
2020-06-15 14:42:42 -0500

    pseudo coded axios request

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuesax from 'vuesax'
 import 'vuesax/dist/vuesax.css'
-import axios from 'axios';
+// import axios from 'axios';
 
 import App from './App.vue'
 import * as VueGoogleMaps from 'vue2-google-maps'
@@ -24,21 +24,21 @@ Vue.use(Vuesax, {
 Vue.config.productionTip = false
 
 new Vue({
-  created: function () {
-    // create an axios get reques to grab the user information and check if they are logged in
-    axios.get('/api/auth/google/login')
-      .then(function (response) {
-        if (response.data.redirect == '/') {
-          alert(`welcome ${response.data.user}`)
-        } else if (response.data.redirect == '/login') {
-          window.location.href = 'api/auth/google';
-        }
-      })
-      .catch(function (error) {
-        alert(error);
-        window.location = "/login"
-      })
-  },
+  // created: function () {
+  //   // create an axios get reques to grab the user information and check if they are logged in
+  //   axios.get('/api/auth/google/login')
+  //     .then(function (response) {
+  //       if (response.data.redirect == '/') {
+  //         alert(`welcome ${response.data.user}`)
+  //       } else if (response.data.redirect == '/login') {
+  //         window.location.href = 'api/auth/google';
+  //       }
+  //     })
+  //     .catch(function (error) {
+  //       alert(error);
+  //       window.location = "/login"
+  //     })
+  // },
   render: h => h(App),
 
 }).$mount('#app')

bb00ee718af2af716a02f85c95fe21cf057874de
JerryMcDonald <jandh62808@gmail.com>
2020-06-16 18:40:58 -0500

    this may fix the clientRun

--- client/vue.config.js
+++ client/vue.config.js
@@ -4,11 +4,18 @@ const { PORT } = require('../server/index')
 module.exports = {
   devServer: {
       host: '0.0.0.0',
-      port: `${PORT}`,
-      public: `13.85.81.104:${PORT}`,
+      port: `8082`,
       proxy: {
-        "/api": {
-          target: `https://localhost:${PORT}`,
+        "/api/auth/google/callback": {
+          target: `http://0.0.0.0:${PORT}`,
+          secure: false,
+        },
+        "/api/auth/google/login": {
+          target: `http://0.0.0.0:${PORT}`,
+          secure: false,
+        },
+        "/api/auth/google": {
+          target: `http://0.0.0.0:${PORT}`,
           secure: false,
         },
       },

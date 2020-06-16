c945fea02fa123f65d12055737900ff788f2fc77
Richard Rosier <jimmyroro58@gmail.com>
2020-06-16 12:22:56 -0500

    alter CreateCrawl to use environmental variables

--- client/src/components/CreateCrawl.vue
+++ client/src/components/CreateCrawl.vue
@@ -25,14 +25,16 @@ export default {
   },
   methods: {
     saveCrawl: function () {
+      console.log(process.env.VUE_APP_MY_IP);
+      console.log(process.env.NODE_ENV);
       const { crawlDate, title } = this.$parent;
       const date = crawlDate.split("T")[0];
       const time = crawlDate.split("T")[1];
-      axios.post('http://localhost:8081/api/crawl/add', {
+      axios.post(`${process.env.VUE_APP_MY_IP}/api/crawl/add`, {
         idCreator: 1,
-        title: title,
-        crawlDate: date,
-        crawlTime: time,
+        title: title || 'badTitle',
+        crawlDate: date || 'badDate',
+        crawlTime: time || 'badTime',
       })
       .catch((err) => {
         console.log(err, 'save crawl in createCrawl');
@@ -44,19 +46,22 @@ export default {
       let locations = [];
       for (let x = 0; x < markers.length; x++) {
         locations.push({
-          name: markers[x].position.name,
-          streetNumber: places[x].address_components[0].long_name,
-          street: places[x].address_components[1].short_name,
-          city: places[x].address_components[3].short_name,
-          state: places[x].address_components[5].short_name,
-          zip: places[x].address_components[7].short_name,
-          lat: markers[x].position.lat,
-          lon: markers[x].position.lng,
-          formatted: places[x].formatted_address,
+          name: markers[x].position.name || null,
+          streetNumber: places[x].address_components[0].long_name || null,
+          street: places[x].address_components[1].short_name || null,
+          city: places[x].address_components[3].short_name || null,
+          state: places[x].address_components[5].short_name || null,
+          zip: places[x].address_components[7].short_name || null,
+          lat: markers[x].position.lat || null,
+          lon: markers[x].position.lng || null,
+          formatted: places[x].formatted_address || null,
         })
       }
       locations.forEach((location) => {
-        axios.post('http://localhost:8081/api/location/add', location)
+        axios.post(`${process.env.VUE_APP_MY_IP}/api/location/add`, location)
+          .then((data) => {
+            console.log(data, 'from post location');
+          })
           .catch((err) => {
             console.log(err, 'error in savelocation in createcrawl')
           })

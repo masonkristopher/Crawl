fe04aaab5490e0b3ac05bb5eb678cf7561e354be
Richard Rosier <jimmyroro58@gmail.com>
2020-06-21 23:34:07 -0500

    gmap only showing markers on join click

--- client/src/components/JoinedCrawl.vue
+++ client/src/components/JoinedCrawl.vue
@@ -1,15 +1,13 @@
 <template>
 <div>
   <h1>Viewing crawl locations for {{ crawlName }}</h1>
-  <button v-on:click="join">Join</button>
   <div>
-    <Map v-bind:userId="userId" v-bind:crawlId="crawlId" />
+    <Map v-bind:userId="userId" v-bind:crawlId="crawlId"/>
   </div>
 </div>
 </template>
 
 <script>
-import axios from "axios";
 import Map from "./Gmap.vue";
 
 export default {
@@ -24,18 +22,6 @@ export default {
       crawlName: this.$route.params.crawlName,
     }
   },
-  methods: {
-    join() {
-      console.log(this.$route.params)
-      axios.post(`/api/crawl/join/${this.crawlId}/${this.userId}`)
-        .then(res => {
-          console.log(res.data);
-        })
-        .catch(error => {
-          console.log(error);
-        })
-    }
-  }
 }
 </script>
 

d06919620a4acd8e846361d37d8a6560e67d0c22
Clayton Christenson <cchristenson.opspark@gmail.com>
2020-06-23 15:06:03 -0500

    add participants component

---
+++ client/src/components/Participants.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>some user</h1>
+    <h1>some user</h1>
+    <h1>some user</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Participants",
+};
+</script>
+
+<style scoped>
+</style>

95151e0afb15249c777d443c0fd458fec2b751e4
Clayton Christenson <cchristenson.opspark@gmail.com>
2020-06-24 18:44:06 -0500

    button rendering in participant list

--- client/src/components/Participant.vue
+++ client/src/components/Participant.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
     <p>{{ participant.Email }}</p>
+    <button @click="{sendtext(7072947750, 'hurry the fuck up!')}">
+      Text 'em hurry the fuck up!
+    </button>
   </div>
 </template>
 
 <script>
+
+import axios from 'axios';
+
 export default {
   name: 'Participant',
   props: ['participant'],
+  methods: {
+    sendtext(to, body) {
+      axios.post('api/twilio', { to, body })
+        .then(() => {
+        // set the status state to 'send which will later trigger a redirect
+          console.log('line22, should mean message sent');
+        })
+        .catch(err => console.error(err));
+    },
+  },
 };
 </script>

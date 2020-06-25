4a88ea7327905253878b9c507be4daec71f67ab9
Clayton Christenson <cchristenson.opspark@gmail.com>
2020-06-25 16:10:48 -0500

    text button only renders if participant.Phone_Number

--- client/src/components/Participant.vue
+++ client/src/components/Participant.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <p>{{ participant.Email }}</p>
-    <button @click="{sendtext(7072947750, 'hurry the fuck up!')}">
-      Text 'em hurry the fuck up!
-    </button>
+    <div v-if="participant.Phone_Number">
+      <button @click="{sendtext(participant.Phone_Number, 'hurry the fuck up!')}">
+        Text 'em hurry the fuck up!
+      </button>
+    </div>
   </div>
 </template>
 
@@ -18,10 +20,11 @@ export default {
     sendtext(to, body) {
       axios.post('api/twilio', { to, body })
         .then(() => {
-        // set the status state to 'send which will later trigger a redirect
           console.log('line22, should mean message sent');
         })
-        .catch(err => console.error(err));
+        .catch((err) => {
+          console.log('this error is because the user has no phone number:', err);
+        });
     },
   },
 };
